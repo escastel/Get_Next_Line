4d1b24fd82aa7152acb42234b2b1976b8e942611
Ester Castellano Rios <escastel@c3r8s5.42malaga.com>
2023-06-08 13:57:04 +0200

    librería

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:13:42 by escastel          #+#    #+#             */
-/*   Updated: 2023/06/08 13:48:10 by escastel         ###   ########.fr       */
+/*   Updated: 2023/06/08 13:56:46 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 42
 # endif
 # include <unistd.h>
 # include <stdlib.h>
